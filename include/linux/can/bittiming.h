cb18c59852f41c7d52c3fcb5a0b3e3914b02d3f3
Vincent Mailhol <mailhol@kernel.org>
2025-01-02 15:31:32 +0900

    can: calc_bittiming: add PWM calculation
    Perform the PWM calculation according to CiA recommendations.
    
    Note that for databitrates greater than 5 MBPS, tqmin is less than
    CAN_PWM_NS_MAX (which is defined to 200 nano seconds), consequently,
    the result of the division:
    
      DIV_ROUND_UP(xl_ns, CAN_PWM_NS_MAX)
    
    is one and thus the for loop automatically stops on the first
    iteration giving a single PWM symbol per bit as expected. Because of
    that, there is no actual need for a separate conditional branch for
    when the databitrate is greater than 5 MBPS.
    
    Signed-off-by: Vincent Mailhol <mailhol@kernel.org>

--- include/linux/can/bittiming.h
+++ include/linux/can/bittiming.h
@@ -180,6 +180,8 @@ int can_calc_bittiming(const struct net_device *dev, struct can_bittiming *bt,
 void can_calc_tdco(struct can_tdc *tdc, const struct can_tdc_const *tdc_const,
 		   const struct can_bittiming *dbt,
 		   u32 tdc_mask, u32 *ctrlmode, u32 ctrlmode_supported);
+
+int can_calc_pwm(struct net_device *dev, struct netlink_ext_ack *extack);
 #else /* !CONFIG_CAN_CALC_BITTIMING */
 static inline int
 can_calc_bittiming(const struct net_device *dev, struct can_bittiming *bt,
@@ -193,6 +195,11 @@ static inline void
 can_calc_tdco(struct can_tdc *tdc, const struct can_tdc_const *tdc_const,
 	      const struct can_bittiming *dbt,
 	      u32 tdc_mask, u32 *ctrlmode, u32 ctrlmode_supported)
+{
+}
+
+static inline int
+can_calc_pwm(struct net_device *dev, struct netlink_ext_ack *extack)
 {
 }
 #endif /* CONFIG_CAN_CALC_BITTIMING */
