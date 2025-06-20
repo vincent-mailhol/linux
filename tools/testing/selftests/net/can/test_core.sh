a3c0c9f475b2c61bc543270d11be894c2464b5f4
Vincent Mailhol <mailhol.vincent@wanadoo.fr>
2025-06-20 14:22:18 +0900

    selftests: can: add can FD support
    Signed-off-by: Vincent Mailhol <mailhol.vincent@wanadoo.fr>

--- tools/testing/selftests/net/can/test_core.sh
+++ tools/testing/selftests/net/can/test_core.sh
@@ -3,11 +3,18 @@
 
 export CANIF=${CANIF:-"vcan0"}
 BITRATE=${BITRATE:-500000}
+DBITRATE=${DBITRATE:-2000000}
+FD=${FD:1}
 
 setup_if()
 {
 	if [[ $CANIF == vcan* ]]; then
 		ip link add name $CANIF type vcan || exit $ksft_skip
+		if [[ $FD == 1 ]]; then
+			ip link set $CANIF mtu 72 || exit $ksft_skip
+		fi
+	elif [[ $FD == 1 ]]; then
+		ip link set dev $CANIF type can bitrate $BITRATE dbitrate $DBITRATE || exit $ksft_skip
 	else
 		ip link set dev $CANIF type can bitrate $BITRATE || exit $ksft_skip
 	fi
